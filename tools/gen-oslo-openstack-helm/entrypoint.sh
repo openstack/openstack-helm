edc5b93d6621292b7e43898c9dab3e0ff5c67336
Pete Birley <pete@port.direct>
2017-04-07 09:18:48 -0500

    Generic Oslo Config Generation

--- tools/gen-oslo-openstack-helm/entrypoint.sh
+++ tools/gen-oslo-openstack-helm/entrypoint.sh
@@ -3,11 +3,19 @@ set -xe
 
 git clone --depth 1 --branch ${PROJECT_BRANCH} ${PROJECT_REPO} /tmp/${PROJECT}
 cd /tmp/${PROJECT}
+
+
+TOX_VENV_DIR=$(crudini --get tox.ini testenv:genconfig envdir | sed 's|^{toxworkdir}|.tox|')
+if [ -z "$TOX_VENV_DIR" ]
+then
+      TOX_VENV_DIR=".tox/genconfig"
+fi
+GENCONFIG_FLAGS=$(crudini --get tox.ini testenv:genconfig commands | sed 's/^oslo-config-generator//')
+
 tox -egenconfig
 
-source .tox/genconfig/bin/activate
+source ${TOX_VENV_DIR}/bin/activate
 
-python /opt/gen-oslo-openstack-helm/generate.py \
-  --config-file config-generator/${PROJECT}.conf \
-  --helm_chart ${PROJECT} \
-  --helm_namespace ${PROJECT}
+python /opt/gen-oslo-openstack-helm/generate.py ${GENCONFIG_FLAGS} \
+    --helm_chart ${PROJECT} \
+    --helm_namespace ${PROJECT}
