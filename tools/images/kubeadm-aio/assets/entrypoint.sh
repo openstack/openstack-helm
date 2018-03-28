0d969910afb20922796e361450944bc3d18c4bf7
portdirect <pete@port.direct>
2018-03-28 00:31:15 -0400

    Kube: Kubernetes v1.10.2 support
    This Ps adds support for kubernetes v1.10.2
    
    Change-Id: Ic1db020cc86fa9db78f3f3cfcda92e3291a889a8

--- tools/images/kubeadm-aio/assets/entrypoint.sh
+++ tools/images/kubeadm-aio/assets/entrypoint.sh
@@ -17,7 +17,7 @@
 set -e
 if [ "x${ACTION}" == "xgenerate-join-cmd" ]; then
 : ${TTL:="10m"}
-DISCOVERY_TOKEN="$(kubeadm token --kubeconfig /etc/kubernetes/admin.conf create --ttl ${TTL} --usages signing --groups '')"
+DISCOVERY_TOKEN="$(kubeadm token --kubeconfig /etc/kubernetes/admin.conf create --ttl ${TTL} --usages signing,authentication --groups '')"
 TLS_BOOTSTRAP_TOKEN="$(kubeadm token --kubeconfig /etc/kubernetes/admin.conf create --ttl ${TTL} --usages authentication --groups \"system:bootstrappers:kubeadm:default-node-token\")"
 DISCOVERY_TOKEN_CA_HASH="$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* /sha256:/')"
 API_SERVER=$(cat /etc/kubernetes/admin.conf | python -c "import sys, yaml; print yaml.safe_load(sys.stdin)['clusters'][0]['cluster']['server'].split(\"//\",1).pop()")
