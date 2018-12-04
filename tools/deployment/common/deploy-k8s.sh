e8e0d30e7bf7897b3c8c9ae934d73c55c21f9067
Steve Wilkerson <wilkers.steve@gmail.com>
2018-12-04 11:03:14 -0600

    Use minikube deployment for single node gates
    This updates the single node jobs to use the recently
    added minikube deployment, with the intent to reduce
    overall runtime for the single node jobs
    
    Change-Id: I6efdbf890d86bf916cef2d1a3b7eba1f6132c2f9

--- tools/deployment/common/deploy-k8s.sh
+++ tools/deployment/common/deploy-k8s.sh
@@ -19,6 +19,5 @@ set -xe
 CURRENT_DIR="$(pwd)"
 : ${OSH_INFRA_PATH:="../openstack-helm-infra"}
 cd ${OSH_INFRA_PATH}
-make dev-deploy setup-host
-make dev-deploy k8s
+./tools/deployment/common/005-deploy-k8s.sh
 cd ${CURRENT_DIR}
