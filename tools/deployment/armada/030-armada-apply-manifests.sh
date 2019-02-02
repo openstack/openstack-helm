dc1ef4c138516cf517911227e73b15f3d490ed3d
Steve Wilkerson <wilkers.steve@gmail.com>
2019-02-02 16:43:14 -0600

    Update openstack-helm Armada job
    This updates the openstack-helm Armada job to instead deploy
    only Ceph, the OpenStack service charts, and their dependencies.
    This is dependent on the addition of the Armada job for Ceph and
    the LMA components to openstack-helm-infra. This also updates the
    jobs definition to use the osh-gate-runner playbook instead, as
    well as sets the job both to a periodic and experimental job
    
    Depends-On: https://review.openstack.org/#/c/634676/
    Depends-On: https://review.openstack.org/#/c/633067/
    Change-Id: I7e191a153f123e04e123acc33fb691d8117062a9

--- tools/deployment/armada/030-armada-apply-manifests.sh
+++ tools/deployment/armada/030-armada-apply-manifests.sh
@@ -16,7 +16,7 @@
 
 set -xe
 
-manifests="armada-cluster-ingress armada-ceph armada-lma armada-osh"
+manifests="armada-cluster-ingress armada-ceph armada-osh"
 for manifest in $manifests; do
   echo "Applying $manifest manifest"
   armada apply /tmp/$manifest.yaml
