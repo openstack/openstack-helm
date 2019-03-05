6186995745f0adedf76890447e6f1aa9dddb3417
Angie Wang <angie.wang@windriver.com>
2019-03-05 15:56:09 -0500

    Add Panko Chart
    This commit adds a helm chart to deploy panko.
    The default deployment for panko is ocata.
    
    Change-Id: I01f447fe0170be64e318885e307b013e30fd4762
    Story: 2005021
    Task: 29500
    Signed-off-by: Angie Wang <angie.wang@windriver.com>

---
+++ tools/deployment/multinode/240-panko.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2019 The Openstack-Helm Authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+set -xe
+
+#NOTE: Wait for deploy
+helm upgrade --install panko ./panko \
+  --namespace=openstack \
+  --set pod.replicas.api=2 \
+  ${OSH_EXTRA_HELM_ARGS} \
+  ${OSH_EXTRA_HELM_ARGS_PANKO}
+
+#NOTE: Wait for deploy
+./tools/deployment/common/wait-for-pods.sh openstack
+
+#NOTE: Validate Deployment info
+helm status panko
+export OS_CLOUD=openstack_helm
+openstack service list
