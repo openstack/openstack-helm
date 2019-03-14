d6a0e0b85c912c478a59d3173ffa0b7392a3f65d
Pete Birley <pete@port.direct>
2019-03-14 15:26:30 -0500

    Gate: Permit ceph deployment from outside the cluster
    This PS extends the gate scripts to allow ceph to be deployed from
    a workstation external to the k8s cluster.
    
    Change-Id: I09b9a11747bab32c19637d8dd076b8caa3b89445
    Signed-off-by: Pete Birley <pete@port.direct>

--- tools/deployment/multinode/kube-node-subnet.sh
+++ tools/deployment/multinode/kube-node-subnet.sh
@@ -16,10 +16,27 @@
 set -e
 
 UTILS_IMAGE=docker.io/openstackhelm/gate-utils:v0.1.0
-NODE_IPS=$(mktemp --suffix=.txt)
+NODE_IPS=$(mktemp)
 kubectl get nodes -o json | jq -r '.items[].status.addresses[] | select(.type=="InternalIP").address' | sort -V > $NODE_IPS
-FIRST_IP_SUBNET=$(sudo docker run --rm ${UTILS_IMAGE} ipcalc "$(head -n 1 ${NODE_IPS})/24" | awk '/^Network/ { print $2 }')
-LAST_IP_SUBNET=$(sudo docker run --rm ${UTILS_IMAGE} ipcalc "$(tail -n 1 ${NODE_IPS})/24" | awk '/^Network/ { print $2 }')
+function run_and_log_ipcalc {
+  POD_NAME="tmp-$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-z | head -c 5; echo)"
+  kubectl run ${POD_NAME} \
+    --generator=run-pod/v1 \
+    --wait \
+    --image ${UTILS_IMAGE} \
+    --restart=Never \
+    ipcalc -- "$1"
+  end=$(($(date +%s) + 900))
+  until kubectl get pod/${POD_NAME} -o go-template='{{.status.phase}}' | grep -q Succeeded; do
+    now=$(date +%s)
+    [ $now -gt $end ] && echo containers failed to start. && \
+        kubectl get pod/${POD_NAME} -o wide && exit 1
+  done
+  kubectl logs pod/${POD_NAME}
+  kubectl delete pod/${POD_NAME}
+}
+FIRST_IP_SUBNET=$(run_and_log_ipcalc "$(head -n 1 ${NODE_IPS})/24" | awk '/^Network/ { print $2 }')
+LAST_IP_SUBNET=$(run_and_log_ipcalc "$(tail -n 1 ${NODE_IPS})/24" | awk '/^Network/ { print $2 }')
 rm -f $NODE_IPS
 function ip_diff {
   echo $(($(echo $LAST_IP_SUBNET | awk -F '.' "{ print \$$1}") - $(echo $FIRST_IP_SUBNET | awk -F '.' "{ print \$$1}")))
