1c4084bdc099493a8c774fc0c3fdd83e8322085c
Randeep Jalli <rj2083@att.com>
2019-04-05 12:18:39 -0400

    add docker-default apparmor profile for prometheus-alertmanager Add in prometheus-alertmanager gate script as a script
    Change-Id: I3c10f9a9d4403fd91da292a50d204f73a9295611

---
+++ tools/deployment/apparmor/050-prometheus-alertmanager.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright 2019 The Openstack-Helm Authors.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -xe
+
+#NOTE: Lint and package chart
+make prometheus-alertmanager
+
+#NOTE: Deploy command
+tee /tmp/prometheus-alertmanager.yaml << EOF
+pod:
+  mandatory_access_control:
+    type: apparmor
+    alertmanager:
+      alertmanager: localhost/docker-default
+storage:
+  enabled: false
+EOF
+helm upgrade --install prometheus-alertmanager ./prometheus-alertmanager \
+    --namespace=osh-infra \
+    --values=/tmp/prometheus-alertmanager.yaml
+
+#NOTE: Wait for deploy
+./tools/deployment/common/wait-for-pods.sh osh-infra
+
+#NOTE: Validate Deployment info
+helm status prometheus-alertmanager
