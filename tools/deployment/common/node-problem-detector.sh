a31bb2b04918107a08cb14201ae72f2b5696cb9d
Steve Wilkerson <sw5822@att.com>
2019-07-02 15:27:08 -0500

    Add node-problem-detector chart
    This adds a chart for the node problem detector. This chart
    will help provide additional insight into the status of the
    underlying infrastructure of a deployment.
    
    Updated the chart with new yamllint checks.
    
    Change-Id: I21a24b67b121388107b20ab38ac7703c7a33f1c1
    Signed-off-by: Steve Wilkerson <sw5822@att.com>

---
+++ tools/deployment/common/node-problem-detector.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -xe
+
+#NOTE: Lint and package chart
+make kubernetes-node-problem-detector
+
+#NOTE: Deploy command
+tee /tmp/kubernetes-node-problem-detector.yaml << EOF
+monitoring:
+  prometheus:
+    pod:
+      enabled: false
+    service:
+      enabled: true
+manifests:
+  service: true
+EOF
+helm upgrade --install kubernetes-node-problem-detector \
+    ./kubernetes-node-problem-detector --namespace=kube-system \
+    --values=/tmp/kubernetes-node-problem-detector.yaml
+
+#NOTE: Wait for deploy
+./tools/deployment/common/wait-for-pods.sh kube-system
+
+#NOTE: Validate Deployment info
+helm status kubernetes-node-problem-detector
