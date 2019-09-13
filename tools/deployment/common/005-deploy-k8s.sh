1d903948aea71164e9a3200c4088cabbcd181389
Tin Lam <tin@irrational.io>
2019-09-13 13:16:59 -0500

    Add label to namespaces
    This patch set adds the ability to add a label to the following namespaces:
    
    - kube-public
    - kube-system
    - default
    
    This was previously done in kubeadm in patch [0] but appears to be
    removed as the script moved around. Also, this PS adds this to the
    minikube deployment.
    
    [0] https://review.opendev.org/#/c/540131/
    
    Change-Id: I0f06e8ae0cd7742313b447dc2d563c7d92318fb0
    Signed-off-by: Tin Lam <tin@irrational.io>

--- tools/deployment/common/005-deploy-k8s.sh
+++ tools/deployment/common/005-deploy-k8s.sh
@@ -235,3 +235,8 @@ kubectl label nodes --all ceph-osd=enabled
 kubectl label nodes --all ceph-mds=enabled
 kubectl label nodes --all ceph-rgw=enabled
 kubectl label nodes --all ceph-mgr=enabled
+
+# Add labels to the core namespaces
+kubectl label --overwrite namespace default name=default
+kubectl label --overwrite namespace kube-system name=kube-system
+kubectl label --overwrite namespace kube-public name=kube-public
