d14d826b2608b07e0ffa4caa085753ed1b0043a4
Gage Hugo <gagehugo@gmail.com>
2020-03-02 11:45:05 -0600

    Remove OSH Authors copyright
    The current copyright refers to a non-existent group
    "openstack helm authors" with often out-of-date references that
    are confusing when adding a new file to the repo.
    
    This change removes all references to this copyright by the
    non-existent group and any blank lines underneath.
    
    Change-Id: I1882738cf9757c5350a8533876fd37b5920b5235

--- tools/deployment/common/005-deploy-k8s.sh
+++ tools/deployment/common/005-deploy-k8s.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Copyright 2017 The Openstack-Helm Authors.
-# Copyright 2019, AT&T Intellectual Property
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
