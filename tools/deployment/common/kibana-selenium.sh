49b50d632bceb2e0ca18e1d7f9ace335148fe562
dmyrhorodskyi <dmyrhorodskyi@mirantis.com>
2020-03-11 17:57:40 +0200

    Fix Kibana Selenium tests
    XPath to expected element was changed after
    Kibana upgrade, this commit changes XPath
    according chnges in new Kibana.
    
    Change-Id: I501de225e1226991db9c263cedf38397cda7b51f

--- tools/deployment/common/kibana-selenium.sh
+++ tools/deployment/common/kibana-selenium.sh
@@ -9,8 +9,8 @@ export KIBANA_USER="admin"
 export KIBANA_PASSWORD="changeme"
 export KIBANA_URI="kibana.osh-infra.svc.cluster.local"
 
-export KERNEL_QUERY="discover?_g=()&_a=(columns:!(_source),index:'kernel-*',interval:auto,query:(match_all:()),sort:!('@timestamp',desc))"
-export JOURNAL_QUERY="discover?_g=()&_a=(columns:!(_source),index:'journal-*',interval:auto,query:(match_all:()),sort:!('@timestamp',desc))"
-export LOGSTASH_QUERY="discover?_g=()&_a=(columns:!(_source),index:'logstash-*',interval:auto,query:(match_all:()),sort:!('@timestamp',desc))"
+export KERNEL_QUERY="discove?r_g=()&_a=(columns:!(_source),index:'kernel*',interval:auto,query:(language:kuery,query:''),sort:!('@timestamp',desc))"
+export JOURNAL_QUERY="discove?r_g=()&_a=(columns:!(_source),index:'journal*',interval:auto,query:(language:kuery,query:''),sort:!('@timestamp',desc))"
+export LOGSTASH_QUERY="discove?r_g=()&_a=(columns:!(_source),index:'logstash*',interval:auto,query:(language:kuery,query:''),sort:!('@timestamp',desc))"
 
 python3 tools/gate/selenium/kibanaSelenium.py
