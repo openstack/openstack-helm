56ed10e637182ffe413a32ecd8bbfa5e3562a6fc
Tin Lam <tin@irrational.io>
2021-02-10 15:56:15 -0600

    chore(doc): updates copyright years
    Now that it is 2021, this patch updates the copyright date to reflect
    the correct years.
    
    Change-Id: I7ce07588d18a463dc256cb6ed87424fc55d4e2c7
    Signed-off-by: Tin Lam <tin@irrational.io>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'openstack-helm'
-copyright = u'2016, OpenStack Foundation'
+copyright = u'2016-2021, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
