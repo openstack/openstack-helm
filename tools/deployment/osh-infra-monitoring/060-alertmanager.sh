24fd882cd6965b6864485f2a9770de6cca11e28f
jayonlau <jayonlau@gmail.com>
2021-10-13 12:58:17 -0400

    Remove helm status from deployment scripts for osh-infra-monitoring
    With the move to helm v3, helm status requires a namespace to be specified, but doing so breaks helm v2 compatability. This change removes the usage of helm serve in openstack-helm-infra's deployment scripts.
    
    Change-Id: I7d17d2ff4a44fc8d16cc653b33253cce536bfce1

--- tools/deployment/osh-infra-monitoring/060-alertmanager.sh
+++ tools/deployment/osh-infra-monitoring/060-alertmanager.sh
@@ -24,6 +24,3 @@ helm upgrade --install prometheus-alertmanager ./prometheus-alertmanager \
 
 #NOTE: Wait for deploy
 ./tools/deployment/common/wait-for-pods.sh osh-infra
-
-#NOTE: Validate Deployment info
-helm status prometheus-alertmanager
