14cfdaf0749f0b0fabab7a518b722cfe21a72cd9
Tin Lam <t@lam.wtf>
2022-04-20 00:07:31 -0500

    fix: update date
    It is now 2022 and the copyright date should reflect that.
    
    Signed-off-by: Tin Lam <tin@irrational.io>
    Change-Id: Id4871590680a95527c58b8e90d5cab7479a43257

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'openstack-helm'
-copyright = '2016-2021, OpenStack Foundation'
+copyright = '2016-2022, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
