1b23f6f6942d32703684c70af5ec82d0b19a7f2f
shyam-biradar <shyam.biradar@trilio.io>
2022-04-30 14:25:32 +0000

    Fixes for wlm cloud trust job and workloads service

---
+++ triliovault/utils/install_tvo_chart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+cd ../../
+helm upgrade --install triliovault ./triliovault --namespace=openstack --values=./triliovault/values_overrides/image_pull_secrets.yaml --values=./triliovault/values_overrides/conf_triliovault.yaml --values=./triliovault/values_overrides/train-ubuntu_bionic.yaml
