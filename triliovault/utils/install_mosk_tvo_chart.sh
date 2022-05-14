1a8f93d72655bc35847eeb0a79b69df84bf0aba0
shyam-biradar <shyam.biradar@trilio.io>
2022-05-14 11:21:06 +0000

    mosk fixes

--- triliovault/utils/install_mosk_tvo_chart.sh
+++ triliovault/utils/install_mosk_tvo_chart.sh
@@ -2,7 +2,7 @@
 
 cd ../../
 helm upgrade --install triliovault ./triliovault --namespace=openstack \
---values=./triliovault/values_overrides/image_pull_secrets.yaml 
+--values=./triliovault/values_overrides/image_pull_secrets.yaml \
 --values=./triliovault/values_overrides/conf_triliovault.yaml \
 --values=./triliovault/values_overrides/victoria-ubuntu_focal.yaml \
 --values=./triliovault/values_overrides/admin_creds.yaml
