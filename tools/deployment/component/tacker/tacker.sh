97c1c61ea2c774ac99fb255511bbf193be9545b0
Ayumu Ueha <ueha.ayumu@fujitsu.com>
2023-07-14 06:15:46 +0000

    Add Tacker chart
    At this point it requires nfs provisioner that provides
    ReadWriteMany volumes for vnfpackages, csar files and
    also the same storage class is used for logs.
    
    Also this patch adds a job that only deploys Tacker but
    does not tests it in any way. This job is put to the experimental
    pipeline.
    
    Co-authored-by: Vladimir Kozhukalov <kozhukalov@gmail.com>
    
    Story: 2010682
    Task: 47771
    Change-Id: I56d7ba489746ab4f818086440a7783f4b1ecb292

---
+++ tools/deployment/component/tacker/tacker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+set -xe
+
+#NOTE: Get the over-rides to use
+: ${OSH_EXTRA_HELM_ARGS_TACKER:="$(./tools/deployment/common/get-values-overrides.sh tacker)"}
+: ${RUN_HELM_TESTS:="no"}
+
+#NOTE: Lint and package chart
+make tacker
+
+#NOTE: Deploy command
+helm upgrade --install tacker ./tacker \
+    --namespace=openstack \
+    ${OSH_EXTRA_HELM_ARGS:=} \
+    ${OSH_EXTRA_HELM_ARGS_TACKER}
+
+#NOTE: Wait for deploy
+./tools/deployment/common/wait-for-pods.sh openstack
